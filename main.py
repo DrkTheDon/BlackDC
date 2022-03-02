d89d45ff31ac423692730f7bd49bca50809c1d04
Darkest Surface <74876947+DrkTheDon@users.noreply.github.com>
2022-03-02 16:37:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,15 @@
 #####################################################
 
 # Imports
+from turtle import clear
+from urllib.parse import uses_fragment
 import discord
 from colorama import Fore
 import time
 import os
 import itertools
 from datetime import datetime
+from discord.ext import commands
 
 # Useful Defines
 def clearcmd():
@@ -21,10 +24,13 @@ def clearcmd():
 
 # Global Variables
 bot = discord.Client()
-TOKEN = "YOUR_TOKEN_HERE" # This is for Debugging/Developing
+TOKEN = "YOUR_TOKEN_HERE" # This is for Debugging (DEVS)
 now = datetime.now()
 curtime = now.strftime("%H:%M")
-
+PREFIX = "drk"
+COMMANDS = {
+    "help", 
+}
 # Main 
 clearcmd()
 
@@ -32,21 +38,25 @@ print(f"{Fore.YELLOW}[*] Initializing")
 time.sleep(3.5)
 clearcmd()
 
-
 @bot.event
 async def on_ready():
-    print(f"{Fore.GREEN}[+]{Fore.LIGHTWHITE_EX} Conntected to {Fore.YELLOW}{bot.user}                                                                  {Fore.LIGHTWHITE_EX}Press {Fore.YELLOW}CTRL + C {Fore.LIGHTWHITE_EX}to quit")
+    print(f"{Fore.GREEN}[+]{Fore.LIGHTWHITE_EX} Conntected to {Fore.YELLOW}{bot.user}                                                                         {Fore.LIGHTWHITE_EX}Press {Fore.YELLOW}CTRL + C {Fore.LIGHTWHITE_EX}to quit")
     time.sleep(0.5)
-    print(f"{Fore.YELLOW}[!]{Fore.LIGHTWHITE_EX} Send {Fore.YELLOW}drk$help{Fore.LIGHTWHITE_EX} to start!\n\n")
+    print(f"{Fore.YELLOW}[!]{Fore.LIGHTWHITE_EX} Send {Fore.YELLOW}drk help{Fore.LIGHTWHITE_EX} to start!")
+    print(f"{Fore.YELLOW}----------------------------------------------------------------------------------{Fore.LIGHTWHITE_EX}")
+
     
 
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
         return
-    if message.content == "drk$help":
-        await message.channel.send(f"@{message.author} This bot is under development!") 
+    if message.content == f"{PREFIX} help":
+        await message.channel.send(f"{message.author.mention} This bot is under development!") 
+        
         print(f"{Fore.BLUE}{curtime} {Fore.YELLOW}[*] {Fore.LIGHTWHITE_EX}Sent {Fore.RED}This bot is under development!{Fore.LIGHTWHITE_EX} in {Fore.YELLOW}#{message.channel} ")
+
+    
 
 # Main Run
 try:
@@ -54,4 +64,9 @@ try:
 except discord.errors.LoginFailure:
     print(f"{Fore.RED}[-]{Fore.LIGHTWHITE_EX} Inproper Token or disabled account.")
     time.sleep(0.5)
+    quit()
+except KeyboardInterrupt:
+    print(f"{Fore.RED}[-]{Fore.LIGHTWHITE_EX} Quitting.")
+    time.sleep(0.5)
+    clearcmd()
     quit()

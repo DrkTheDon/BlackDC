a8ab8e7e4f800cb2df8045e3c7717d0c55c4e133
Darkest Surface <74876947+DrkTheDon@users.noreply.github.com>
2022-03-06 17:52:31 +0100

    Added Batch Instlaler for Windows Machines

--- installers/install.bat
+++ installers/install.bat
@@ -1,1 +1,9 @@
-
+@echo off
+pip install colorama
+pip install discord
+pip install requests
+pip install discord.py
+pip install numpy
+cls
+echo Packages have been installed, you cna close this window
+pause

974d05941eb4ec50b14cc5147b8d31b87f975146
Darkest Surface <74876947+DrkTheDon@users.noreply.github.com>
2022-03-07 08:44:33 +0100

    Updated comments

--- installers/install.sh
+++ installers/install.sh
@@ -7,7 +7,7 @@
 ## NOTE: This will install all the dependicies     ##
 #####################################################
 
-# Variables
+# Global Variables
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
 GREEN="\033[0;32m"

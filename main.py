aba4c09e41a9545aef1453b12315ad5c3256d744
Darkest Surface <74876947+DrkTheDon@users.noreply.github.com>
2022-07-10 01:01:04 +0200

    Updated Test ign command

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ async def help(ctx):
 @bot.command()
 async def test(ctx):
     if ctx.author != bot.user:
-        return
+        print(f"{Fore.BLUE}{curtime} {Fore.YELLOW}[!] {Fore.LIGHTWHITE_EX}Ignored command from{Fore.RED} {ctx.author} {Fore.LIGHTWHITE_EX}In {Fore.YELLOW}#{ctx.channel} ")
     await ctx.message.delete()
     await ctx.send(f"{ctx.author.mention} BlackDC is working!", delete_after=1.5)
     print(f"{Fore.BLUE}{curtime} {Fore.YELLOW}[*] {Fore.LIGHTWHITE_EX}BlackDC is {Fore.RED}Working{Fore.LIGHTWHITE_EX} sent in {Fore.YELLOW}#{ctx.channel} ")
